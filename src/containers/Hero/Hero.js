a8137b1f7436946c3ed44c949d9e67521d8bc339
Lasma Poksane <laasmapoksane@gmail.com>
2022-04-27 18:18:56 +0200

    add animation to h1 tag with aos

--- src/containers/Hero/Hero.js
+++ src/containers/Hero/Hero.js
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "../../components/UI/Button/Button";
 import classes from "./Hero.module.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Hero = () => {
   let initialText = "Hi. I am Lasma.";
   const [heroHeading, setHeroHeading] = useState(initialText);
 
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
     <div className={classes.Hero}>
       <div className={classes.HeroText}>
         <h1
+          data-aos="zoom-in"
+          data-aos-duration="2000"
           className={classes.HeroHeading}
           onMouseOver={() => setHeroHeading("A front-end web developer.")}
           onMouseLeave={() => setHeroHeading(initialText)}
