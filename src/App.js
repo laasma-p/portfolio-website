91fbfd7b1f1ece85acc7f47dd63297293975cb7a
Lasma Poksane <laasmapoksane@gmail.com>
2022-04-27 18:10:57 +0200

    Add Hero component with particles background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Particles from "react-tsparticles";
+import Hero from "./containers/Hero/Hero";
 
-function App() {
+const particlesOptions = {
+  fpsLimit: 60,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: false,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  fullScreen: {
+    enable: false,
+    zIndex: 0,
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 700,
+      },
+      value: 90,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Particles
+        id="tsparticles"
+        className="particles"
+        params={particlesOptions}
+      />
+      <Hero />
     </div>
   );
-}
+};
 
 export default App;
