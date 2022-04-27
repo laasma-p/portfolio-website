531a0797bb7f00696e5e268c884ad1e2e1079166
Lasma Poksane <laasmapoksane@gmail.com>
2022-04-27 18:09:15 +0200

    implement text change on hover event

--- src/containers/Hero/Hero.js
+++ src/containers/Hero/Hero.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Button from "../../components/UI/Button/Button";
 import classes from "./Hero.module.css";
 
 const Hero = () => {
   let initialText = "Hi. I am Lasma.";
+  const [heroHeading, setHeroHeading] = useState(initialText);
 
   return (
     <div className={classes.Hero}>
       <div className={classes.HeroText}>
-        <h1 className={classes.HeroHeading}>{initialText}</h1>
+        <h1
+          className={classes.HeroHeading}
+          onMouseOver={() => setHeroHeading("A front-end web developer.")}
+          onMouseLeave={() => setHeroHeading(initialText)}
+        >
+          {heroHeading}
+        </h1>
       </div>
 
       <div className={classes.Actions}>
